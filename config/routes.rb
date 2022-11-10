9aa48e7e27197db982f555166f77ee2a7e44961b
juliette barbay <barbayjuliette95@gmail.com>
2022-11-10 16:10:38 +0800

    Created all the user stories for restaurants, review action missing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: 'restaurants#index'
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:show, :new, :create]
+  end
 end
